40ea9f96a77afbde59dd7cba72c05ac1e08bc5e0
Shem <oduor2shem@gmail.com>
2025-06-05 14:09:21 +0300

    feat: update BookList component layout and styling for improved user experience

--- src/Components/Booklist.jsx
+++ src/Components/Booklist.jsx
@@ -3,9 +3,9 @@ import BookItem from './BookItem.jsx';
 
 function BookList({ books }) {
   return (
-    <ul className="list-none p-0"> 
+    <ul className='flex justify-center items-center min-h-20 bg-gray-100 p-4'> 
       {books.length === 0 ? (
-        <p className="text-center text-gray-500 italic mt-8">No books in the collection yet. Add some!</p>
+        <p className="text-center text-gray-500 italic mt-8 flex justify-center items-center">No books in the collection yet. Add some!</p>
       ) : (
         books.map(book => (
           <BookItem key={book.id} book={book} />
